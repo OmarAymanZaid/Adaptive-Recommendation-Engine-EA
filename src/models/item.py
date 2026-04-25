b22eccfd3f5693cf073323f9a439de1ee8c123d1
OmarAyman <omaraymanzaid1122@gmail.com>
2026-04-25 20:30:39 +0300

    Refactor ItemIndividual and UserIndividual classes to remove predict method

--- src/models/item.py
+++ src/models/item.py
@@ -5,7 +5,3 @@ class ItemIndividual(BaseIndividual):
     def __init__(self, vector, item_id=None):
         super().__init__(vector)
         self.item_id = item_id
-
-    def predict(self, user_vector):
-        """Optional (symmetry, not always needed)"""
-        return float(np.dot(self.vector, user_vector))
--- src/models/user.py
+++ src/models/user.py
@@ -5,7 +5,3 @@ class UserIndividual(BaseIndividual):
     def __init__(self, vector, user_id=None):
         super().__init__(vector)
         self.user_id = user_id  # Link to dataset
-
-    def predict(self, item_vector):
-        """Predict rating using dot product"""
-        return float(np.dot(self.vector, item_vector))
