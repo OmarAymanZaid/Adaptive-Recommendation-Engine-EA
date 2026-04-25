0028846f55111dce9c446e56dc99faadca0ee5af
OmarAyman <omaraymanzaid1122@gmail.com>
2026-04-25 20:14:03 +0300

    Implement ItemIndividual class with prediction method

--- src/models/item.py
+++ src/models/item.py
@@ -1,1 +1,11 @@
-# item
+from models.individuals import BaseIndividual
+import numpy as np
+
+class ItemIndividual(BaseIndividual):
+    def __init__(self, vector, item_id=None):
+        super().__init__(vector)
+        self.item_id = item_id
+
+    def predict(self, user_vector):
+        """Optional (symmetry, not always needed)"""
+        return float(np.dot(self.vector, user_vector))
