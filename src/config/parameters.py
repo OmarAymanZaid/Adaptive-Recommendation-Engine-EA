3f8b987a39fc29e89ba739959c77d473ac26e248
OmarAyman <omaraymanzaid1122@gmail.com>
2026-04-25 20:26:08 +0300

    Add parameters configuration for genetic algorithm

--- src/config/parameters.py
+++ src/config/parameters.py
@@ -1,1 +1,17 @@
-# parameters
+PARAMS = {
+    "population_size": 50,
+    "num_generations": 100,
+    "vector_dim": 10,
+
+    "selection_method": "tournament",
+    "crossover_method": "one_point",
+    "mutation_method": "gaussian",
+
+    "mutation_rate": 0.1,
+    "crossover_rate": 0.8,
+
+    "init_method": "uniform",
+    "elitism": True,
+
+    "random_seed": 42,
+}
